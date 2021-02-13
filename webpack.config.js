a83bceb1e7f7b0431bf40ee666336e8ae1902791
abdoecomerce <abdoelprince12345@gmail.com>
2021-02-13 12:23:40 -0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: "",
+    filename: "main.js",
+  },
+
+  mode: "development",
+
+  devServer: {
+    contentBase: path.join(__dirname, "/dist"),
+    port: 8080,
+    writeToDisk: true,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              // optimize : true
+            },
+          },
+        ],
+      },
+
+      /*   {
+        test: /\.css$/,
+        use: ["style-loader", miniCssExtractPlugin.loader, "css-loader"],
+      }, */
+
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: miniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+  
+    new htmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new miniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+
+    new optimizeCssAssetsPlugin({}),
+  ],
+};
